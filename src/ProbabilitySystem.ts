96be5f60c75c42cbbc5899b00044ccf7a239ef31
Lucas Yang <yangchenshin77@gmail.com>
2026-01-13 03:31:22 +0800

    Refactor spin method to enhance symbol checking logic and maintainability

--- src/ProbabilitySystem.ts
+++ src/ProbabilitySystem.ts
@@ -1,5 +1,22 @@
 export class ProbabilitySystem {
+  reels: string[][] = [
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', '10', 'J'],
+  ]
+
   spin(betLine: string) {
-    return betLine === 'L1' ? 20 : 0
+    const firstSymbols = new Set<string>()
+    for (const reel of this.reels) {
+      firstSymbols.add(reel[0])
+    }
+
+    if (firstSymbols.size === 1 && betLine === 'L1') {
+      return 20
+    }
+
+    return 0
   }
 }
