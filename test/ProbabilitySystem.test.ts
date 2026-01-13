4ec04960e954384d99ee7023965f787cb39fd270
Lucas Yang <yangchenshin77@gmail.com>
2026-01-14 03:50:35 +0800

    Update tests description

--- test/ProbabilitySystem.test.ts
+++ test/ProbabilitySystem.test.ts
@@ -2,12 +2,12 @@ import { describe, expect, test } from 'vitest'
 import { ProbabilitySystem } from '../src/ProbabilitySystem'
 
 describe('probability system', () => {
-  test('lose', () => {
+  test('Row 1 hit, bet L2 -> 0', () => {
     const sut = new ProbabilitySystem()
     expect(sut.spin('L2')).toBe(0)
   })
 
-  test('L1 hot -> 20', () => {
+  test('Row 1 hit, bet L1 -> 20', () => {
     const sut = new ProbabilitySystem()
     expect(sut.spin('L1')).toBe(20)
   })
